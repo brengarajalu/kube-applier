b35c813f0cdca5ff04a6caa58115f7ce0aa4fb17
Greg Lyons <glyons@box.com>
2017-07-27 16:33:27 -0700

    Upgrade to new release using quick runs

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 # only one top level directory.
 name=kube-applier
 
-# 0.1.2b is equivalent to the official 0.1.2 release here
+# 0.1.2c is equivalent to the official 0.1.2 release here
 # https://github.com/box/kube-applier/tree/v0.1.2
-tarname=v0.1.2b.tar.gz
+tarname=v0.1.2c.tar.gz
 
 set -e
 set -x
